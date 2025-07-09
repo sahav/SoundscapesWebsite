c95b0abe40f5457616d34d7af5386add28a4d1ef
mfmckenna <megan.mckenna@gmail.com>
2025-07-09 14:08:24 -0700

    minor error

--- code/summary_ONMS-GCP-all.R
+++ code/summary_ONMS-GCP-all.R
@@ -48,6 +48,11 @@ colnames(lookup) <- lookup[1, ]  # Set first row as column names
 lookup <- as.data.frame( lookup[-1, ] ) # Remove the first row
 colnames(lookup)[5] = "NCEI"
 
+inFile = paste0(outDirP, "//early_nrs_datasets.xlsx")
+NRSold = as.data.frame ( openxlsx :: read.xlsx(inFile) )
+NRSold$Start_Date = as.Date(NRSold$Start_Date, origin = "1899-12-30")
+NRSold$End_Date = as.Date(NRSold$End_Date, origin = "1899-12-30")
+
 # LIST SUB DIRECTORIES ####
 #these should be the "monitoring sites" you want to gather information about
 command = "gsutil"
@@ -81,7 +86,8 @@ if ( length(tmp)  > 0 ) {
  
 # GET INFORMATION FROM METADATA FILES ####
 # loads one file at a time from GCP, no saving to local machine 
-#ONMS + SS
+
+## ONMS + SS ####
 subdirsALL = c(subdirsONMS, subdirsSS) 
 output = NULL
 for (s in 1:length(subdirsALL) ) { # s = 1
@@ -155,26 +161,119 @@ for (s in 1:length(subdirsALL) ) { # s = 1
   }
 }
 ouputTest = output # output = ouputTest
-# SAVE SUMMARY ####
+### FORMAT OUTPUT ####
 output = as.data.frame(output)
 colnames(output) = c("Path", "DeploymentNumber", "DeploymentName", "Instrument", "Start_Date", "End_Date","Lat","Lon")
 output$Site = basename((output$Path))
 output$Start_Date = as.Date(output$Start_Date, format = "%Y-%m-%d")
 output$End_Date = as.Date(output$End_Date, format = "%Y-%m-%d")
-## ADD INFO from context file ####
+### ADD INFO from context file ####
 lookup_selected = lookup %>% select(NCEI, Region,`Common Name/Identifiers`,`Site Description/Driver for Monitoring Location Choice`)
 output = left_join(output, lookup_selected, by = c("Site" = "NCEI"), relationship = "many-to-many")
 output$Duration = difftime( output$End_Date, output$Start_Date,"days")
-
-## SAVE all sites ####
 output$Project [is.na(output$Region)] = "SanctSound" 
 output$Project [!is.na(output$Region)] = "ONMS-sound" 
-save(output, file = paste0(outDirP, "\\data_gantt_ONMS_gantt_ALL", DC, ".Rda") ) #all data
-## SAVE only long-term monitoring sites ####
-cat("Not long-term sites...", setdiff(unique(output$Site), unique(lookup$NCEI)) ) #only long-term monitoring sites
-outputT = output[!is.na(output$Region),] 
-save(outputT, file = paste0(outDirP, "\\data_gantt_ONMS_gantt_", DC, ".Rda") )
-write.csv(outputT, file = paste0(outDirP, "\\data_gantt_ONMS_gantt_", DC, ".csv") )
+names(output )
+
+## NRS ####
+outNRS = NULL
+subdirsALL = subdirsNRS 
+for (s in 1:length(subdirsALL) ) { # s=1
+  
+  ## read in files
+  args = c("ls", "-r", subdirsALL[s])
+  sFiles = ( system2(command, args, stdout = TRUE, stderr = TRUE)  )
+  json_files = grep("\\.json$", sFiles, value = TRUE) #metadata files
+  cat("Processing... ", dirNames[s], "[", s, " of ", length(dirNames),"]", length(sFiles), "\n" )
+  
+  for (jf in 1:length( json_files) ) { # jf = 1
+    
+    #path for this deployment metadata 
+    path_parts <- unlist(strsplit(json_files[jf], "/"))
+    deploymentPath <- paste(path_parts[1:(length(path_parts) - 2)], collapse = "/")
+    
+    url = paste0("https://storage.googleapis.com/", 
+                 gsub ("gs://", '', paste(json_files[jf], collapse = "") ) )
+    h = curl(url, "r")
+    json_content = readLines(url)
+    close(h)
+    tmp = fromJSON(paste(url, collapse = ""))
+    
+    ## check which version
+    if ( is.null( tmp$deployment$audio_start) ) { #not small caps
+      
+      name  = tmp$SITE 
+      deploy = tmp$DEPLOYMENT_NAME
+      instr = tmp$INSTRUMENT_TYPE
+      start = as.Date( gsub("T"," ", tmp$DEPLOYMENT$AUDIO_START), format = "%Y-%m-%d")
+      end   = as.Date( gsub("T"," ", tmp$DEPLOYMENT$AUDIO_END), format = "%Y-%m-%d")
+      lat   = tmp$DEPLOYMENT$DEPLOY_LAT
+      lon   = tmp$DEPLOYMENT$DEPLOY_LON
+      
+      #save to output data - each deployment
+      outNRS = rbind(outNRS, c(deploymentPath, jf, name, deploy, instr, 
+                               as.character(start), as.character(end), 
+                               lat, lon) )
+      
+      
+    } else if ( !is.null( tmp$deployment$audio_start) ) { #small caps
+      
+      name  = tmp$site 
+      names(tmp)[names(tmp) == "INSTRUMENT_TYPE"] =  "instrument_type"
+      instr = tmp$instrument_type
+      deploy =  tmp$deployment_id
+      start = as.Date( gsub("T"," ", tmp$deployment$audio_start), format = "%Y-%m-%d")
+      end   = as.Date( gsub("T"," ", tmp$recover$audio_end), format = "%Y-%m-%d")
+      lat   = tmp$deployment$lat
+      lon   = tmp$deployment$lon
+      
+      #save to output data - each deployment
+      outNRS = rbind(outNRS, c(deploymentPath, jf, name, deploy, instr, 
+                               as.character(start), as.character(end), 
+                               lat, lon) )
+    } else {
+      cat(deploymentPath, "- Not correct format", "\n" )
+      
+      outNRS = rbind(outNRS, c(deploymentPath, jf, NA, NA, 
+                               NA, NA, 
+                               NA, NA) )
+      
+    } 
+  }
+}
+#### FORMAT OUTPUT ####
+ouputTest = output # output = ouputTest
+output = as.data.frame(output)
+colnames(output) = c("Path", "FileCount", "SiteName", "DeploymentName", "Instrument", "Start_Date", "End_Date","Lat","Lon")
+output$Site = paste0(basename((output$Path)))
+output$Start_Date = as.Date(output$Start_Date, format = "%Y-%m-%d")
+output$End_Date = as.Date(output$End_Date, format = "%Y-%m-%d")
+output$Days = difftime( output$End_Date, output$Start_Date,"days")
+output$Instrument = "AUH"
+# ADD OLD DEPLOYMENTS
+if (projectN == "NRS"){ output2 = rbind(output, NRSold )}
+output2$Site = paste0( tolower( projectN ), "_", output2$SiteName ) 
+output = output2
+output$Site = gsub("_", "", output$Site)
+### ADD INFO from context file ####
+lookup_selected = lookup %>% select(NCEI, Region,`Common Name/Identifiers`,`Site Description/Driver for Monitoring Location Choice`)
+output = left_join(output, lookup_selected, by = c("Site" = "NCEI"), relationship = "many-to-many")
+output$Duration = difftime( output$End_Date, output$Start_Date,"days")
+output$Project [is.na(output$Region)]  = "NRS" 
+output$Project [!is.na(output$Region)] = "ONMS-sound" 
+
+# COMBINE DATASETS ####
+outputC 
+## SAVE ALL ####
+save(outputC, file = paste0(outDirP, "\\data_gantt_ONMS_gantt_ALL", DC, ".Rda") ) #all data
+
+## SAVE ONMS long-term only ####
+cat("Not long-term sites...", setdiff(unique(outputC$Site), unique(lookup$NCEI)) ) #only long-term monitoring sites
+outputONMS = outputC[!is.na(outputC$Region),] 
+
+outputONMS
+save(outputONMS,      file = paste0(outDirP, "\\data_gantt_ONMS_gantt_", DC, ".Rda") )
+write.csv(outputONMS, file = paste0(outDirP, "\\data_gantt_ONMS_gantt_", DC, ".csv") )
 
 # GANTT CHART  ####
 # load(file = paste0(outDirP, "\\data_gantt_ONMS_gantt_2025-04-28.Rda") )
