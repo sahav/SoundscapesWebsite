e4f9cd5ced2937e94b5abda0c0462c1284d0c21c
Emma Beretta - NOAA Affiliate <emma.beretta@noaa.gov>
2025-10-13 17:24:43 -0700

    gr01 plots

--- code/plot_ONMS-conditions.R
+++ code/plot_ONMS-conditions.R
@@ -17,6 +17,7 @@ library(ggplot2)
 library(tidyverse)
 library(openxlsx)
 library(reshape)
+
 library(gtable)
 library(grid)
 library(ggtext)
@@ -25,7 +26,7 @@ library(viridis)
 
 #SITES ####
 # ONMSsites = c("sb01", "sb03", "hi01", "hi03", "hi04", "hi08", "pm01", "as01", "mb01", "mb02", "oc02", "cb11" )
-ONMSsites = c("fk05")
+ONMSsites = c("gr01")
 
 ## directories ####
 outDir   =  "C:/Users/embe5980/SoundscapesWebsite/" # your local git repo 
